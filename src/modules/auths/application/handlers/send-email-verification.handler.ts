3dc9e8a5831904fdf29ef7f4c6d57db58cc55017
Hafizul Furqan <hafizulf68@gmail.com>
2025-10-17 19:02:47 +0700

    fix: modify message to send email verification

--- src/modules/auths/application/handlers/send-email-verification.handler.ts
+++ src/modules/auths/application/handlers/send-email-verification.handler.ts
@@ -2,6 +2,7 @@ import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { SendEmailVerificationCommand } from "../command/send-email-verification.command";
 import { RabbitMQPublisher } from "../../infrastructure/rabbitmq/rabbitmq.publisher";
 import { EMAIL_EXCHANGE, EMAIL_ROUTING } from "../../infrastructure/rabbitmq/email-routing";
+import { SendEmailVerificationPayload } from "../../interface/dto/auth-verification.dto";
 
 @CommandHandler(SendEmailVerificationCommand)
 export class SendEmailVerificationHandler 
@@ -12,8 +13,8 @@ implements ICommandHandler<SendEmailVerificationCommand, void> {
   
   async execute(command: SendEmailVerificationCommand): Promise<void> {
     const { email, purpose, expiresAt, verifyUrl } = command.payload;
-    const message = {
-      to: email,
+    const message: SendEmailVerificationPayload = {
+      email: email,
       purpose: purpose,
       verifyUrl: verifyUrl,
       expiresAt: expiresAt,
